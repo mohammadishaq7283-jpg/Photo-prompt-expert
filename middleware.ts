27526be2a5918c21f37cee4aa00c493812a437d6
mohammadishaq7283-jpg <mohammadishaq7283@gmail.com>
2026-03-04 21:03:35 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,18 @@
 import createMiddleware from 'next-intl/middleware';
  
 export default createMiddleware({
+  // A list of all locales that are supported
   locales: ['en', 'ur', 'hi', 'ar', 'es', 'fr'],
-  defaultLocale: 'en'
+ 
+  // Used when no locale matches
+  defaultLocale: 'en',
+
+  // Force redirect to default locale always
+  localePrefix: 'always'
 });
  
 export const config = {
-  matcher: ['/', '/(en|ur|hi|ar|es|fr)/:path*']
+  // Match only internationalized pathnames
+  // Yeh regex zaroori hai taake api routes aur images block na hon
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
 };
