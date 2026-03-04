e370a031b6c574a1db70050f62363c8dc137f615
mohammadishaq7283-jpg <mohammadishaq7283@gmail.com>
2026-03-04 20:57:11 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 const createNextIntlPlugin = require('next-intl/plugin');
+
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Agar images use kar rahe hain to domains allow karein
+  images: {
+    domains: ['oaidalleapiprodscus.blob.core.windows.net'], // DALL-E images ke liye
+  },
+};
 
 module.exports = withNextIntl(nextConfig);
